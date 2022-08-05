9178c6a6370efe66a2f50b61fe42e32964f5c81b
rencewang <47406212+rencewang@users.noreply.github.com>
2022-08-05 01:10:57 -0700

    remind

---
+++ script.js
@@ -0,0 +1,150 @@
+remember 
+
+that you are loved
+你把自己照顾的那么好
+即便如此也没关系
+活成这样的大小孩 并不可耻 即便如此
+我就是我自己的神 倔强
+红颜会知己，知音不会难觅 闪光的回忆
+人回来就会离开
+每个人只能陪你走一段路
+迷茫的时候走难走的路
+你不需要去刻意迎合。真实的你，就很好。
+我们每个人都在努力跨过所有不如意和悲伤。终究他会逆流成河，我们却会克服而徜徉其中，一路逆风。
+
+我的粉底和眼霜真的很贵，每一滴泪水都要留给值得的人。“
+
+但风向是会转变的，终有一天，会吹向更有光亮的地方
+
+The world will break your heart ten ways to Sunday, that’s guaranteed.
+
+The world is a fine place and worth fighting for.
+
+Some day when you get where you're going, you'll look around and you will know that it was you and the people who love you who put you there — and that will be the greatest feeling in the world
+
+Most things don’t exist. It’s just a time saving expression for a collection of financial interests
+
+walking though a lot of rainstorms makes you clean
+
+there’s not much of a difference between a stadium full of cheering fans, and an angry crowd screaming abuse at you. They’re both just making a lot of noise. How you take it is up to you. Convince yourself they’re cheering for you. You do that, and someday, they will. Glee
+
+part of the journey is the end end game
+
+we have to choose despite uncertainty first reformed
+
+“The currents before us are ever changing, we must adapt and press forward if we are to see our journey’s end.“ 
+— Civilization
+
+生活不可能像你想象得那么好，
+但也没有你想象得那么糟。 
+人的脆弱和坚强都超乎自己的想象。 
+有时，可能一句话就泪流满面；
+有时，回过头也发现自己咬着牙走了很长的路。
+
+自己的选择要自己负责　没有任何抱歉　
+死的人就一直死了，可我们还要活下去
+
+哪有什么岁月静好
+有人替你负重前行
+
+你当时相信的那些事情，
+会在如今变成美丽风景
+
+20年后的我还是我，不一样的烟火
+
+化好妆继续前行
+
+想好了就去做
+
+you are what you love not who loves you
+
+you just might find who you're supposed to be fifteen
+you will find some one someday who will actually treat you well white horse
+
+that grades don't really matter
+that it doesn't really matter all that much if you feel misunderstood by the people who don't know you, as long as you feel understood by the people who do know you, who will show up for you, who see you as human beings
+that nobody gives a shit so go live the life that you've always wanted to live
+
+that they only know what you let them see
+that you've got your mother's love
+that there are so many things, to be thankful for
+that you are the one you've been waiting for, all of your life
+that if everything is not fine in the end, then it's not the end
+that you miss all the shots you don't take
+
+
+to be honest what I liked were the things I didn't know
+when it rains it pours
+to breathe
+to take a break 
+to self care
+to be alone
+to call your parents
+to do 10 pushups today, even if it's 12am
+to reminesce about suburbia
+to go forward, even if alone
+you;re not alone
+to not rush into things
+there is always hope
+to stay inspired by the unanswerable
+
+in each breath there is death and forever
+
+it's time to let go of this endless summer afternoon
+
+God helps those who help themselves
+
+breathe in breathe out and decide 
+
+to fall down often, so that we can look at the sky
+
+that I'm happy, I'm happy for you
+
+Go west, life is peaceful there
+
+That everything comes from consummation, and everything comes with consequence 
+
+that someday what's in front of you will be behind you
+
+there is so much you don't know
+
+where you cannot see, everything is happening chinese
+
+Once the light fades everything is dark
+
+it's still life
+
+he’s got my past frozen behind glass
+But I’ve got me
+
+Don’t be afraid to jump then fall
+
+bring my flowers now while I'm living
+
+once the light fades everything is dark
+
+more will go with age
+
+时间会回答成长，
+成长会回答梦想
+
+on a hard dry surface, you have to engrave the truth
+
+Keep the sun the moon the stars in your eyes
+
+you can run free forever
+
+it feels different when you're with me
+
+your greatest stories are yours to tell
+
+In times of great opportunity, don’t fall for opportunism
+
+every now and then, pause, look around, drink it in. 'Cause this is it. And it might all be gone tomorrow.
+
+君子之交淡如水
+平平淡淡才是真
+
+色禁重，香禁重，这世间万物，皆是淡胜于浓
+
+我与他终究都是尘世中人，上天赐给我们最好的天时地利，只在眼下这一瞬。我若抓不住，到了明日，世事浮云，人心惟危，皆是瞬息万变，也许他不再爱我，我不再爱他，那时候翻思今日的面红心热辗转反侧，只会遗憾错失了良机
