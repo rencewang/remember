b0e01085d545f9f9560e471c4c829ba4b7e98a03
rencewang <47406212+rencewang@users.noreply.github.com>
2022-08-05 11:14:39 -0700

    reformat lines

---
+++ remember.js
@@ -0,0 +1,384 @@
+export const remember = [
+  {
+    prep: 'that',
+    line: 'you are loved',
+  },
+  {
+    prep: 'that',
+    line: '你把自己照顾得那么好，愿你遇见另一个自由而独立的灵魂，分享彼此的世界。即便没有遇见，也是很好的：你终究是自由的',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '即便如此，也没关系',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '我就是我自己的神，在我活的地方',
+    translation: '',
+    attribution: '倔强',
+  },
+  {
+    prep: 'that',
+    line: '红颜会知己，知音不会难觅',
+    translation: '',
+    attribution: '闪光的回忆',
+  },
+  {
+    prep: 'that',
+    line: '你就是你自己的神，在你活的地方',
+    translation: '',
+    attribution: '',
+  },
+  {
+    prep: 'that',
+    line: '人回来，就会离开',
+    translation: '',
+    attribution: '路过人间',
+  },
+  {
+    prep: 'that',
+    line: '每个人只能陪你走一段路',
+    translation: '',
+    attribution: '山河故人',
+  },
+  {
+    prep: 'that',
+    line: '迷茫的时候走难走的路',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '你不需要去刻意迎合，真实的你，就很好',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '我们每个人都在努力跨过所有不如意和悲伤。终究他会逆流成河，我们却会克服而徜徉其中，一路逆风',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '我的粉底和眼霜真的很贵，每一滴泪水都要留给值得的人',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '但风向是会转变的，终有一天，会吹向更有光亮的地方',
+    translation: '',
+    attribution: 'Genshin Impact',
+  },
+  {
+    prep: 'that',
+    line: 'the world will break your heart ten ways to Sunday, that’s guaranteed',
+    attribution: 'Silver Linings Playbook',
+  },
+  {
+    prep: 'that',
+    line: 'the world is a fine place and worth fighting for',
+    attribution: 'Ernest Hemingway',
+  },
+  {
+    prep: 'that',
+    line: "some day when you get where you're going, you'll look around and you will know that it was you and the people who love you who put you there — and that will be the greatest feeling in the world",
+    attribution: 'Taylor Swift',
+  },
+  {
+    prep: 'that',
+    line: 'the worst of times are behind us now',
+  },
+  {
+    prep: 'that',
+    line: "most things don't exist. The Ford Motor Company hardly exists. It's just a time-saving expression for a collection of financial interests",
+    attribution: 'Succession',
+  },
+  {
+    prep: 'that',
+    line: 'walking through a lot of rainstorms makes you clean',
+    attribution: 'Taylor Swift',
+  },
+  {
+    prep: 'that',
+    line: 'there’s not much of a difference between a stadium full of cheering fans, and an angry crowd screaming abuse at you - they’re both just making a lot of noise. How you take it is up to you. Convince yourself they’re cheering for you. You do that, and someday, they will',
+    attribution: 'Glee',
+  },
+  {
+    prep: 'that',
+    line: 'part of the journey is the end',
+    attribution: 'Avengers: Endgame',
+  },
+  {
+    prep: 'to',
+    line: "choose despite uncertainty, but don't lose hope",
+    attribution: 'First Reformed',
+  },
+  {
+    prep: 'that',
+    line: '生活不可能像你想象得那么好，但也没有你想象得那么糟。 人的脆弱和坚强都超乎自己的想象。 有时，可能一句话就泪流满面；有时，回过头也发现自己咬着牙走了很长的路。',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '自己的选择要自己负责，没有任何抱歉',
+    translation: '',
+    attribution: '不想放开',
+  },
+  {
+    prep: 'that',
+    line: '死的人就一直死了，可我们还要活下去',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '哪有什么岁月静好，有人替你负重前行',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: '你当时相信的那些事情，会在如今变成美丽风景',
+    translation: '',
+    attribution: '顽固',
+  },
+  {
+    prep: 'that',
+    line: '20年后的你还是你，不一样的烟火',
+    translation: '',
+    attribution: '我就是我',
+  },
+  {
+    prep: 'to',
+    line: '化好妆，继续前行',
+    translation: '',
+  },
+  {
+    prep: 'to',
+    line: '想好了，就去做',
+    translation: '',
+  },
+  {
+    prep: 'that',
+    line: 'you are what you love not who loves you',
+    attribution: 'Save Rock and Roll',
+  },
+  {
+    prep: 'that',
+    line: "you just might find who you're supposed to be",
+    attribution: 'Fifteen',
+  },
+  {
+    prep: 'that',
+    line: 'you will find someone someday who will actually treat you well',
+    attribution: 'White Horse',
+  },
+  {
+    prep: 'that',
+    line: "grades don't really matter",
+  },
+  {
+    prep: 'that',
+    line: "it doesn't really matter all that much if you feel misunderstood by the people who don't know you, as long as you feel understood by the people who do know you, who will show up for you, who see you as human beings",
+    attribution: 'Taylor Swift',
+  },
+  {
+    prep: 'that',
+    line: "nobody gives a shit, so go live the life that you've always wanted to live",
+  },
+  {
+    prep: 'that',
+    line: 'they only know what you let them see',
+    attribution: 'Saturday Nights',
+  },
+  {
+    prep: 'that',
+    line: "you've got your mother's love",
+    attribution: 'When Will My Life Begin (Reprise)',
+  },
+  {
+    prep: 'that',
+    line: 'there are so many things to be thankful for',
+    attribution: 'When Will My Life Begin (Reprise)',
+  },
+  {
+    prep: 'that',
+    line: "you are the one you've been waiting for, all of your life",
+    attribution: 'Show Yourself',
+  },
+  {
+    prep: 'that',
+    line: "if everything is not fine in the end, then it's not the end",
+  },
+  {
+    prep: 'that',
+    line: "you miss all the shots you don't take",
+  },
+  {
+    prep: 'that',
+    line: 'when it rains it pours, so stay thirsty like before',
+    attribution: 'The Kids Aren’t Alright',
+  },
+  {
+    prep: 'to',
+    line: 'breathe',
+  },
+  {
+    prep: 'to',
+    line: 'take a break',
+  },
+  {
+    prep: 'to',
+    line: 'care for yourself',
+  },
+  {
+    prep: 'to',
+    line: 'not get cold',
+  },
+  {
+    prep: 'to',
+    line: "lie on the bed listen to Lana Del Rey's Love",
+  },
+  {
+    prep: 'to',
+    line: 'seek happiness',
+  },
+  {
+    prep: 'to',
+    line: 'practice acceptance',
+  },
+  {
+    prep: 'to',
+    line: 'call your parents',
+  },
+  {
+    prep: 'that',
+    line: 'your mom is growing older too',
+    attribution: 'Never Grow Up',
+  },
+  {
+    prep: 'to',
+    line: "memorize what it sounds like when your dad gets home; remember the footsteps, the words said, your little brother's favorite songs",
+    attribution: 'Never Grow Up',
+  },
+  {
+    prep: 'to',
+    line: 'breathe',
+  },
+  {
+    prep: 'to',
+    line: "do 10 pushups even if it's 12am",
+  },
+  {
+    prep: 'to',
+    line: 'reminisce about suburbia',
+  },
+  {
+    prep: 'to',
+    line: 'go forward, even if alone',
+    attribution: 'Europa Universalis 4',
+  },
+  {
+    prep: 'that',
+    line: 'you are not alone',
+  },
+  {
+    prep: 'to',
+    line: 'not rush into things',
+  },
+  {
+    prep: 'to',
+    line: 'stay inspired by the unanswerable',
+  },
+  {
+    prep: 'to',
+    line: 'let go of that endless summer afternoon',
+    attribution: 'Hard Feelings/Loveless',
+  },
+  {
+    prep: 'to',
+    line: 'breathe in, breathe out, and decide',
+    attribution: "Grey's Anatomy",
+  },
+  {
+    prep: 'to',
+    line: 'look at the sky',
+  },
+  {
+    prep: 'to',
+    line: 'take a picture of some clouds',
+  },
+  {
+    prep: 'that',
+    line: "I'm happy, I'm happy for you",
+    attribution: '(what i wish just one person would say to me)',
+  },
+  {
+    prep: 'that',
+    line: 'everything comes with consequence',
+  },
+  {
+    prep: 'that',
+    line: "someday what's in front of you will be behind you",
+  },
+  {
+    prep: 'that',
+    line: "there is so much you don't know",
+  },
+  {
+    prep: 'that',
+    line: '在你看不见的地方，一切都在发生',
+    translation: 'where you cannot see, everything is happening',
+  },
+  {
+    prep: 'to',
+    line: "bring my flowers now while I'm living",
+    attribution: 'Bring My Flowers Now',
+  },
+  {
+    prep: 'that',
+    line: '时间会回答成长，成长会回答梦想',
+    translation:
+      'time will answer growing up, growing up will answer your dream',
+    attribution: '入海',
+  },
+  {
+    prep: 'that',
+    line: 'on a hard dry substance, you have to engrave the truth',
+    attribution: "Don't Allow the Lucid Moment to Dissolve",
+  },
+  {
+    prep: 'that',
+    line: 'you can run free forever',
+  },
+  {
+    prep: 'that',
+    line: "it feels different when you're with me",
+    attribution: 'Heartbreak Weather',
+  },
+  {
+    prep: 'to',
+    line: 'keep the sun the moon the stars in your eyes',
+    attribution: 'Chasing Thunder',
+  },
+  {
+    prep: 'that',
+    line: 'your greatest stories are yours to tell',
+    attribution: 'Chasing Thunder',
+  },
+  {
+    prep: 'to',
+    line: 'every now and then, pause, look around, drink it in',
+    attribution: "Grey's Anatomy",
+  },
+  {
+    prep: 'that',
+    line: '我与他终究都是尘世中人，上天赐给我们最好的天时地利，只在眼下这一瞬。我若抓不住，到了明日，世事浮云，人心惟危，皆是瞬息万变，也许他不再爱我，我不再爱他，那时候翻思今日的面红心热辗转反侧，只会遗憾错失了良机',
+    translation:
+      "He and I are not saints. Fate has given us the best of times in this moment. If I cannot hold on to this moment, when tomorrow comes, everything might change. Maybe he won't love me, I won't love him. At that time, when I think of today's tossing and turning, anticipation and passion, I will only regret how much we missed.",
+  },
+  {
+    prep: 'that',
+    line: '色禁重，香禁重，这世间万物，皆是淡胜于浓',
+    translation:
+      'refrain from strong scents and strong passions, all things in this world come better light than strong',
+  },
+];
